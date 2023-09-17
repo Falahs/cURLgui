3a8d189ac67a03789b36bedab60e02f6c8d54f6f
Falahs <Falahs@outlook.com>
2023-09-17 20:52:31 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        CmdRun = New Button()
+        txtInput = New TextBox()
+        txtOutput = New TextBox()
+        SuspendLayout()
+        ' 
+        ' CmdRun
+        ' 
+        CmdRun.Location = New Point(12, 12)
+        CmdRun.Name = "CmdRun"
+        CmdRun.Size = New Size(63, 23)
+        CmdRun.TabIndex = 0
+        CmdRun.Text = "Run"
+        CmdRun.UseVisualStyleBackColor = True
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(81, 12)
+        txtInput.Multiline = True
+        txtInput.Name = "txtInput"
+        txtInput.ScrollBars = ScrollBars.Vertical
+        txtInput.Size = New Size(601, 23)
+        txtInput.TabIndex = 1
+        txtInput.Text = "curl -svo /dev/null https://www.fastly.com/"
+        ' 
+        ' txtOutput
+        ' 
+        txtOutput.Location = New Point(12, 41)
+        txtOutput.Multiline = True
+        txtOutput.Name = "txtOutput"
+        txtOutput.ScrollBars = ScrollBars.Both
+        txtOutput.Size = New Size(776, 397)
+        txtOutput.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtOutput)
+        Controls.Add(txtInput)
+        Controls.Add(CmdRun)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    ' Declare form components as class members
+    Friend WithEvents CmdRun As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtOutput As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize the form when it loads
+    End Sub
+
+    Private Sub CmdRun_Click(sender As Object, e As EventArgs) Handles CmdRun.Click
+        ' This event handler is called when the "Run" button is clicked
+
+        ' Clear the output TextBox
+        txtOutput.Clear()
+
+        ' Get the command from the input TextBox
+        Dim command As String = txtInput.Text.Trim()
+
+        ' Check if the command is empty
+        If String.IsNullOrEmpty(command) Then
+            MessageBox.Show("Please enter a command.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Create a new process to run the command
+        Dim process As New Process()
+        process.StartInfo.FileName = "cmd.exe"
+        process.StartInfo.Arguments = "/C " & command
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.RedirectStandardOutput = True
+        process.StartInfo.RedirectStandardError = True
+        process.StartInfo.CreateNoWindow = True
+
+        ' Add an event handler for reading the output data
+        AddHandler process.OutputDataReceived, AddressOf OutputDataReceived
+        AddHandler process.ErrorDataReceived, AddressOf ErrorDataReceived
+
+        ' Start the process
+        process.Start()
+
+        ' Begin asynchronous reading of the output stream
+        process.BeginOutputReadLine()
+        process.BeginErrorReadLine()
+    End Sub
+
+    Private Sub OutputDataReceived(sender As Object, e As DataReceivedEventArgs)
+        ' This event handler is called when data is received from the standard output
+
+        If e.Data IsNot Nothing Then
+            ' Display the output data in the output TextBox
+            AppendText(txtOutput, e.Data & Environment.NewLine)
+        End If
+    End Sub
+
+    Private Sub ErrorDataReceived(sender As Object, e As DataReceivedEventArgs)
+        ' This event handler is called when data is received from the standard error
+
+        If Len(e.Data) > 0 Then
+            ' Ignore Empty Data
+            AppendText(txtOutput, e.Data & Environment.NewLine)
+        End If
+    End Sub
+
+    ' Helper function to append text to a TextBox
+    Private Sub AppendText(textBox As TextBox, text As String)
+        If textBox.InvokeRequired Then
+            textBox.Invoke(Sub() AppendText(textBox, text))
+        Else
+            textBox.AppendText(text)
+        End If
+    End Sub
+
+    Private Sub txtOutput_TextChanged(sender As Object, e As EventArgs) Handles txtOutput.TextChanged
+
+    End Sub
+End Class
